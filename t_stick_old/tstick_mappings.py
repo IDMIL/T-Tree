d4a466c62977e2a14f1478ea8f40b17eb88eab48
Paul Buser <buser.paul@gmail.com>
2022-06-14 15:47:26 -0400

    Black format

--- t_stick_old/tstick_mappings.py
+++ t_stick_old/tstick_mappings.py
@@ -2,35 +2,37 @@ from collections import namedtuple
 from enum import Enum
 
 
-Message = namedtuple('Message', ['name', 'data'])
+Message = namedtuple("Message", ["name", "data"])
 
 # Mappings for the Soprano T-Stick 2G 012.
 class Codes:
     codes = {
-    # Two bytes, indicating serial number and firmware revision
-            0: 'info',
-    # Six bytes, for six different areas of t-Stick?
-            1: 'touch',
-    # Unused.
-            2: 'jab',
-    # Unused.
-            3: 'tap',
-    # Between 5 and 11 bytes.
-    # First three are X/Y/Z of acceleration,
-    # then an optional 6 bytes for gyro (rotation) data,
-    # then one byte for pressure and one byte for piezo.
-            4: 'periodic',
-            }
+        # Two bytes, indicating serial number and firmware revision
+        0: "info",
+        # Six bytes, for six different areas of t-Stick?
+        1: "touch",
+        # Unused.
+        2: "jab",
+        # Unused.
+        3: "tap",
+        # Between 5 and 11 bytes.
+        # First three are X/Y/Z of acceleration,
+        # then an optional 6 bytes for gyro (rotation) data,
+        # then one byte for pressure and one byte for piezo.
+        4: "periodic",
+    }
     # Don't know why these were chosen as the delimiter and the escape character, respectively.
     # But here they are.
     delimiter = 100
     escape = 101
+
 
 class ParserState(Enum):
     UNKNOWN = 0
     AWAITING_INFO = 1
     AWAITING_DATA = 2
     ESCAPED = 3
+
 
 class TStickParser:
     def parse(self, data: bytes):
