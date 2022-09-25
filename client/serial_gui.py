f1f7b2247d498b2b4bf58e1dd734ae80d7ed375f
Paul Buser <buser.paul@gmail.com>
2022-09-24 20:35:41 -0400

    Clean up and put main code in a thread.

--- client/serial_gui.py
+++ client/serial_gui.py
@@ -5,31 +5,31 @@ from time import sleep
 # Third-party libraries
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')   # Add a touch of color
-message_box = sg.Multiline(autoscroll=True, auto_refresh=True, disabled=True, size=(80,10))
-# All the stuff inside your window.
-layout = [  [sg.Text('Received messages')],
-            [message_box],
-            [sg.Text('Send message:'), sg.InputText(), sg.Button('Send', key='send_btn')],
-]
-
 def window_function():
+    sg.theme('DarkAmber')   # Add a touch of color
+    message_box = sg.Multiline(autoscroll=True, auto_refresh=True, disabled=True, size=(80,10))
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Received messages')],
+                [message_box],
+                [sg.Text('Send message:'), sg.InputText(), sg.Button('Send', key='send_btn')],
+                [sg.Button('Quit')],
+    ]
     # Create the Window
     window = sg.Window('Serial Communicator', layout)
     while True:
         event, values = window.read() 
         print(event, values)   
-        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'Quit': # if user closes window or clicks cancel
             break
         print('You entered ', values[0])
 
     window.close()
 
-w = threading.Thread(target=window_function)
-w.start()
 
-sleep(2)
-message_box.print('hello')
+def main():
+    w = threading.Thread(target=window_function)
+    w.start()
 
-sleep(1)
-message_box.print('world')
+
+if __name__ == '__main__':
+    main()
