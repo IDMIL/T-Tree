a1dfcdab96fb8f7b565ee436d6364516b399c375
Paul Buser <buser.paul@gmail.com>
2022-11-15 14:17:49 -0500

    Receiving messages from older T-Stick now works

--- wired/tstick_mappings.py
+++ wired/tstick_mappings.py
@@ -1,7 +1,9 @@
 # Standard libraries
 from collections import namedtuple
 from enum import Enum
-
+from time import sleep
+import queue
+import threading
 
 Message = namedtuple("Message", ["name", "data"])
 
@@ -12,10 +14,7 @@ class Codes:
         1: "touch", # Six bytes, for six different areas of t-Stick?
         2: "jab", # ?
         3: "tap", # ?
-        # Between 5 and 11 bytes.
-        # First three are X/Y/Z of acceleration,
-        # then an optional 6 bytes for gyro (rotation) data,
-        # then one byte for pressure and one byte for piezo.
+        # 10 bytes: 2 each for Y/P/R, then 2 for pressure, 2 for piezo
         4: "periodic",
     }
     delimiter = 100
@@ -30,30 +29,48 @@ class ParserState(Enum):
 
 
 class TStickParser:
-    def parse(self, data: bytes):
+    def __init__(self):
         self.state = ParserState.UNKNOWN
-        messages = []
+        self.data_q = queue.SimpleQueue()
+        self.subscribers = []
+        threading.Thread(target=self.parse_thread, daemon=True).start()
+
+    def subscribe(self, callback):
+        self.subscribers.append(callback)
+
+    def publish_message(self, message: Message):
+        for callback in self.subscribers:
+            callback(message)
+
+    def enqueue_data(self, data: bytes):
+        self.data_q.put(data)
+
+    def parse_thread(self):
         message = None
-        for bite in data:
-            if self.state == ParserState.UNKNOWN:
-                if bite == Codes.delimiter:
-                    self.state = ParserState.AWAITING_INFO
-            elif self.state == ParserState.AWAITING_INFO:
-                if bite in Codes.codes:
-                    message = Message(Codes.codes[bite], [])
-                else:
-                    message = Message("unknown", [])
-                self.state = ParserState.AWAITING_DATA
-            elif self.state == ParserState.AWAITING_DATA:
-                if bite != Codes.delimiter:
+        while True:
+            if self.data_q.empty():
+                sleep(0.01)
+                continue
+            data = self.data_q.get()
+            for bite in data:
+                if self.state == ParserState.UNKNOWN:
+                    if bite == Codes.delimiter:
+                        self.state = ParserState.AWAITING_INFO
+                elif self.state == ParserState.AWAITING_INFO:
+                    if bite in Codes.codes:
+                        message = Message(Codes.codes[bite], [])
+                    else:
+                        message = Message("unknown", [])
+                    self.state = ParserState.AWAITING_DATA
+                elif self.state == ParserState.AWAITING_DATA:
+                    if bite != Codes.delimiter:
+                        message.data.append(bite)
+                    elif bite == Codes.escape:
+                        self.state = ParserState.ESCAPED
+                    else:
+                        self.publish_message(message)
+                        message = None
+                        self.state = ParserState.AWAITING_INFO
+                elif self.state == ParserState.ESCAPED:
                     message.data.append(bite)
-                elif bite == Codes.escape:
-                    self.state = ParserState.ESCAPED
-                else:
-                    messages.append(message)
-                    message = None
-                    self.state = ParserState.AWAITING_INFO
-            elif self.state == ParserState.ESCAPED:
-                message.data.append(bite)
-                self.state = ParserState.AWAITING_DATA
-        return messages
+                    self.state = ParserState.AWAITING_DATA
