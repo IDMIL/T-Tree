581c7123e48febda37e2a6b4a4d834a8d56c6cc9
Paul Buser <buser.paul@gmail.com>
2022-11-15 14:17:49 -0500

    Receiving messages from older T-Stick now works
    Former-commit-id: a1dfcdab96fb8f7b565ee436d6364516b399c375

---
+++ wired/tstick_serial.py
@@ -0,0 +1,28 @@
+# Local libraries
+from tstick_mappings import TStickParser
+
+# Third-party libraries
+from pythonosc.udp_client import SimpleUDPClient
+import serial
+
+READ_SIZE = 16
+
+def create_callback(client):
+    def callback(message):
+        client.send_message(f'/wiredtstick/{message.name}', message.data)
+    return callback
+
+
+def main():
+    ip = "127.0.0.1"
+    port = 1337
+    client = SimpleUDPClient(ip, port)  # Create client
+    parser = TStickParser()
+    parser.subscribe(create_callback(client))
+    ser = serial.Serial('COM5', 115200)
+    ser.write(b's')
+    while True:
+        parser.enqueue_data(ser.read(READ_SIZE))
+
+if __name__ == '__main__':
+    main()
