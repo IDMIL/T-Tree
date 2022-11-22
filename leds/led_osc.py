68397c594d032758ed6335d463a6b1fd6ed7ed18
Paul Buser <buser.paul@gmail.com>
2022-11-22 15:14:23 -0500

    Add code for setting individual LEDs

--- leds/led_osc.py
+++ leds/led_osc.py
@@ -21,16 +21,55 @@ class OscLed:
 
     def _create_osc_server(self):
         dispatcher = Dispatcher()
-        dispatcher.map('/led_ring', self.led_ring_handler)
+        dispatcher.map('/led_ring/set_ring/*', self.led_ring_handler)
+        dispatcher.map('/led_ring/set_led/*/*', self.individual_led_handler)
         return BlockingOSCUDPServer((IP, PORT), dispatcher)
 
-    def led_ring_handler(self, _, *args):
-        if len(args) != 4 or type(args[0]) is not int or type(args[1]) is not float or type(args[2]) is not float or type(args[3]) is not float:
-            print(f'invalid args received: {args}')
+    def individual_led_handler(self, address, *args):
+        address = address.split('/')
+        try:
+            led_num = int(address[-1])
+            if led_num < 0 or led_num >= self.ring.leds_per_ring:
+                raise ValueError
+        except ValueError:
+            print(f'address of led must be an int between 0 and {self.ring.leds_per_ring}. received: {address[-1]}')
             return
+        try:
+            ring_num = int(address[-2])
+            if ring_num < 0 or ring_num >= self.ring.num_rings:
+                raise ValueError
+        except ValueError:
+            print(f'address of ring must be an int between 0 and {self.ring.num_rings}. received: {address[-2]}')
+            return
+        if len(args) != 3:
+            print(f'this handler requires 3 float values (r, g, b). received: {args}')
+            return
+        for i in range(len(args)):
+            if type(args[i]) is not float or args[i] < 0.0 or args[i] > 1.0:
+                print(f'(r, g, b) values must all be floats in [0, 1]. received {args}')
+                return
         c = Color()
-        c.set_rgb(args[1:])
-        ring_num = args[0]
+        c.set_rgb(args)
+        self.ring.set_led(ring_num, led_num, c)
+
+    def led_ring_handler(self, address, *args):
+        address = address.split('/')
+        try:
+            ring_num = int(address[-1])
+            if ring_num < 0 or ring_num >= self.ring.num_rings:
+                raise ValueError
+        except ValueError:
+            print(f'address of ring must be an int between 0 and {self.ring.num_rings}. received: {address[-1]}')
+            return
+        if len(args) != 3:
+            print(f'this handler requires 3 float values (r, g, b). received: {args}')
+            return
+        for i in range(len(args)):
+            if type(args[i]) is not float or args[i] < 0.0 or args[i] > 1.0:
+                print(f'(r, g, b) values must all be floats in [0, 1]. received {args}')
+                return
+        c = Color()
+        c.set_rgb(args)
         self.ring.set_ring(ring_num, c)
 
 
