a3ca164d1067d42b6091194d15efbcf8c9956302
Paul Buser <buser.paul@gmail.com>
2023-01-12 17:32:04 -0500

    A lil refactor

--- wired/tstick_osc.py
+++ wired/tstick_osc.py
@@ -6,6 +6,8 @@ from pythonosc.udp_client import SimpleUDPClient
 import serial
 
 READ_SIZE = 16
+BAUDRATE = 57600
+DEFAULT_PORT = 'COM5'
 
 def create_callback(client):
     def callback(message):
@@ -19,7 +21,7 @@ def main():
     client = SimpleUDPClient(ip, port)  # Create client
     parser = TStickParser()
     parser.subscribe(create_callback(client))
-    ser = serial.Serial('COM5', 115200)
+    ser = serial.Serial(DEFAULT_PORT, BAUDRATE)
     ser.write(b's')
     while True:
         parser.enqueue_data(ser.read(READ_SIZE))
