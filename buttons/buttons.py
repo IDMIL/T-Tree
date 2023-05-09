a4c5c42f4b762b25a882baaeb590bf3f1d4b12dc
Paul Buser <paul.buser@mail.mcgill.ca>
2023-05-09 16:37:46 -0400

    Make fun sounds
    Former-commit-id: 8c4072f14fc0dc06f62df2b943b25e80dc0567fe

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -1,24 +1,33 @@
 # Standard libraries
 from signal import pause
+from subprocess import Popen
 
 # Third-party libraries
 from gpiozero import Button, LED
 
 
 class ArcadeButton:
-    def __init__(self, button_pin, led_pin):
+    def __init__(self, button_pin, led_pin, command=None):
         self.button = Button(button_pin)
         self.led = LED(led_pin)
-        self.button.when_pressed = self.led.on
-        self.button.when_released = self.led.off
+        self.command = command
+        self.button.when_pressed = self.when_pressed
+        self.button.when_released = self.when_released
+
+    def when_pressed(self):
+        self.led.on()
+        if self.command:
+            Popen(self.command)
+
+    def when_released(self):
+        self.led.off()
 
 
 def main():
-    green = ArcadeButton(5, 12)
-    yellow = ArcadeButton(6, 16)
-    blue = ArcadeButton(13, 20)
-    red = ArcadeButton(19, 21)
-
+    green = ArcadeButton(5, 12, ['aplay', 'triangle.wav'])
+    yellow = ArcadeButton(6, 16, ['aplay', 'guitar.wav'])
+    blue = ArcadeButton(13, 20, ['aplay', 'cowbell.wav'])
+    red = ArcadeButton(19, 21,  ['aplay', '808.wav'])
     pause()
 
 
