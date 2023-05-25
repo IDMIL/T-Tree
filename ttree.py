21130545c65b532a5bb4f47731fe29a4a546967f
Paul Buser <buser.paul@gmail.com>
2023-05-25 11:46:42 -0400

    Add some boilerplate to ttree.py Move led_testing.py to utils/

--- ttree.py
+++ ttree.py
@@ -1,1 +1,10 @@
-pass
+from collections import namedtuple
+from typing import Optional
+
+class Branch(namedtuple):
+    index: int
+    port: int
+    paired_to: Optional[str]
+
+def create_branches(num_branches=4, starting_port=8000):
+    return (Branch(i, starting_port+i, None) for i in range(num_branches))
