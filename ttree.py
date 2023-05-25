1a53dae968a4d9e089fa8cf31d96226b5c897821
Paul Buser <buser.paul@gmail.com>
2023-05-25 12:06:13 -0400

    Add basic submodule loading

--- ttree.py
+++ ttree.py
@@ -1,10 +1,18 @@
-from collections import namedtuple
-from typing import Optional
+# Standard libraries
+from importlib import import_module
+from typing import NamedTuple, Optional
 
-class Branch(namedtuple):
+from puara_serial_manager import puara_serial_manager
+
+class Branch(NamedTuple):
     index: int
     port: int
     paired_to: Optional[str]
 
 def create_branches(num_branches=4, starting_port=8000):
     return (Branch(i, starting_port+i, None) for i in range(num_branches))
+
+
+
+if __name__ == '__main__':
+    print(puara_serial_manager.DATA_START)
