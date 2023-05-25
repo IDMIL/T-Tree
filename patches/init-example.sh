2c106a6a42c4f0ab97d870291266c87cca117ad2
Paul Buser <buser.paul@gmail.com>
2023-05-25 12:55:32 -0400

    Organize patches. Add noise patch

--- patches/init-example.sh
+++ patches/init-example.sh
@@ -1,1 +1,1 @@
-pd -nogui -send "port 8000" -send "name TStick_191" -send "pd dsp 1" -open "./init_patch.pd"
+pd -nogui -send "port 8000" -send "name TStick_191" -send "pd dsp 1" -open "./sine_patch.pd"
