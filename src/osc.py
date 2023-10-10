26c94736a50ccab579d56fedda78a059f51c1671
Oren Ronen <oren2ronen@gmail.com>
2023-10-10 17:41:45 -0400

    port

--- src/osc.py
+++ src/osc.py
@@ -2,12 +2,12 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 
 def handle_data(unused_addr, args, data):
-    print(f"Received data: {data}")
+    print(f"Received data2: {data}")
 
 dispatcher = dispatcher.Dispatcher()
 dispatcher.map("/pressure", handle_data, "Data")
 
-server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 8005), dispatcher)
+server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 8001), dispatcher)
 print("Serving on {}".format(server.server_address))
 server.serve_forever()
 
