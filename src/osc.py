1260901538b2bfacc14d1185d08e43141b31e953
Oren Ronen <oren2ronen@gmail.com>
2023-10-10 17:40:00 -0400

    port change

--- src/osc.py
+++ src/osc.py
@@ -10,3 +10,5 @@ dispatcher.map("/pressure", handle_data, "Data")
 server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 8005), dispatcher)
 print("Serving on {}".format(server.server_address))
 server.serve_forever()
+
+#nothing
